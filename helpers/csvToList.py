5b580c1e5651782e5067acecf14133739509fba2
schulace <schulace2@gmail.com>
2016-02-26 20:01:33 -0500

    initial commit

---
+++ helpers/csvToList.py
@@ -0,0 +1,16 @@
+def csvToList(strIn):
+    counter = 0
+    if "\n" in strIn:
+        while (strIn[counter] != "\n"):
+            counter += 1
+
+
+
+
+
+
+
+
+    else:
+        raise("String inputted is not in csv format")
+        return None
