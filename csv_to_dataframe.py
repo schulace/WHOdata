5b292f4c48e87da26d3c1087f732d3b50884110e
schulace <schulace2@gmail.com>
2016-03-08 01:14:35 -0500

    started work on csv interpreter

--- csv_to_dataframe.py
+++ csv_to_dataframe.py
@@ -1,5 +1,5 @@
 import csv
-#  import pandas as pd
+import pandas as pd
 import operator
 
 
@@ -7,9 +7,12 @@ def get_csv_array(file_location):
     fileIn = open(file_location)
     reader = csv.reader(fileIn)
     data_arr = list(reader)
-    year_loc = operator.indexOf('YEAR',data_arr[0])
-    country_loc = operator.indexOf('COUNTRY', data_arr[0])
-    value_loc = operator.indexOf('Numeric', data_arr[0])
+    first_line = data_arr[0]
+    year_loc = data_arr[0].index('YEAR')
+    country_loc = data_arr[0].index('COUNTRY')
+    value_loc = data_arr[0].index('Numeric')
     unsorted_data = []
+
     for x in range(1, len(data_arr)):
-        unsorted_data[x-1] = [data_arr[x][country_loc], data_arr[x][year_loc], data_arr[x][value_loc]]
+        unsorted_data.append([data_arr[x][country_loc], data_arr[x][year_loc], data_arr[x][value_loc]])
+    print(unsorted_data)
--- test.py
+++ test.py
@@ -2,21 +2,22 @@ import fetcher.variableFetcher
 import interpreter.responseSpecifier
 import csv_to_dataframe
 
-def getAPIvar():
-    varDict = fetcher.variableFetcher.getAPIVariables() #grabs a dictionary of descriptions as keys, and API variables as values
+
+def get_api_var():
+    variable_dictionary = fetcher.variableFetcher.getAPIVariables()
     print("type in what you want information on. (ex. mortality, tobacco, infant, or something more specific)")
-    answers = interpreter.responseSpecifier.getMatches(input(), varDict, True)
+    answers = interpreter.responseSpecifier.getMatches(input(), variable_dictionary, True)
     print(answers)
     return answers
 
-def getCountryAbrev():
-    countryDict = fetcher.variableFetcher.getCountryLabels()
+
+def get_country_abbreviation():
+    country_dictionary = fetcher.variableFetcher.getCountryLabels()
     print("type in the country you want data for")
-    answers = interpreter.responseSpecifier.getMatches(input(), countryDict, True)
+    answers = interpreter.responseSpecifier.getMatches(input(), country_dictionary, True)
     print(answers)
     return answers
 
 
-
-# print('http://apps.who.int/gho/athena/api/GHO/' + str(getAPIvar()) + ".csv") # + ".csv?filter=COUNTRY:" + str(countryAbrev
-getAPIvar()
+# print('http://apps.who.int/gho/athena/api/GHO/' + str(getAPIvar()) + ".csv")
+csv_to_dataframe.get_csv_array('test_data.csv')
